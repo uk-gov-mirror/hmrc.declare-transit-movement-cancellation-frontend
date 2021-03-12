9d8f8fa3b2076a86a360aa0ee6771cc58d685e44
kenneth.rayner <kenneth.rayner@digital.hmrc.gov.uk>
2021-03-12 10:55:28 +0000

    CTCTRADERS-1820: add link for support

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -30,8 +30,6 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
 
   val analyticsToken: String = configuration.get[String](s"google-analytics.token")
   val analyticsHost: String = configuration.get[String](s"google-analytics.host")
-  val reportAProblemPartialUrl = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
-  val reportAProblemNonJSUrl = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
   val betaFeedbackUrl = s"$contactHost/contact/beta-feedback"
   val betaFeedbackUnauthenticatedUrl = s"$contactHost/contact/beta-feedback-unauthenticated"
   val signOutUrl: String             = configuration.get[String]("urls.logout")
